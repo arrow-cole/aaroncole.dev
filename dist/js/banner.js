0abc71b99cb0699fd7d9ace6505786a5688fe28e
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2025-02-07 17:33:57 -0500

    Update Banner

--- dist/js/banner.js
+++ dist/js/banner.js
@@ -1,7 +1,7 @@
 // Control banner visibility based on the `data-visible` attribute
         document.addEventListener("DOMContentLoaded", () => {
             const banner = document.getElementById("banner");
-            const isVisible = banner.getAttribute("data-visible") === "true";
+            const isVisible = banner.getAttribute("data-visible") === "false";
 
                 banner.style.display = isVisible ? "block" : "none"; // Hide the banner if not visible
             });
